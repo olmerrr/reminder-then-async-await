1144cb36b0995d2c2fb5b208d7d0f283e17da561
Yaroslav Dombrovskyi <dombrovskiy91@gmail.com>
2024-11-01 11:33:40 +0100

    add post request
    add post request

--- fetch.js
+++ fetch.js
@@ -22,4 +22,35 @@ async function fetchUsers(url) {
 //     .then((response) => response.json())
 //     .then((users2) => console.log(users2));
 // }
-fetchUsers(BASE_URL);
+// fetchUsers(BASE_URL);
+
+// POST
+const body = {
+  name: "Yaroslav",
+  age: 33,
+};
+
+function sendRequest(method, url, body) {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  return fetch(`${url}/users`, {
+    method,
+    body: JSON.stringify(body),
+    headers,
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return response.json().then((err) => {
+        const error = new Error("Error!");
+        error.data(err);
+        throw error;
+      });
+    }
+  });
+}
+sendRequest("POST", BASE_URL, body)
+  .then((data) => console.log(data))
+  .catch((err) => console.log("Error: ", err));
